ec1faf9bb129d2ad58052ea71757408ce29a39da
Jugal Patel <jugalpatel@Jugals-MacBook-Air.local>
2025-07-16 06:16:53 +0530

    added code for express server and some utils

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "app.js";
 
 dotenv.config({ path: ".env" }); //starts from the root directory
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.port || 8000, () => {
+      console.log(`Server is running at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDB connection ERROR`);
+  });
