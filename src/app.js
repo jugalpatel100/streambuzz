0631b3e487acbb902a0fa437fa5183a544c1f22a
Jugal Patel <jugalpatel@Jugals-MacBook-Air.local>
2025-07-17 20:30:06 +0530

    added controller and router for /register

--- src/app.js
+++ src/app.js
@@ -7,25 +7,29 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
+    // origin: [`http://localhost:${process.env.PORT}`],
     credentials: true,
   })
 );
-
 app.use(
   express.json({
     limit: "16kb",
   })
 );
-
 app.use(
   express.urlencoded({
     extended: true,
     limit: "16kb",
   })
 );
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+//import router obj from route
+import userRouter from "./routes/user.route.js";
+
+//define route
+app.use("/api/v1/user", userRouter);
+//http://localhost:3000/api/v1/users/<route_name>
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import { app } from "app.js";
+import { app } from "./app.js";
 
-dotenv.config({ path: ".env" }); //starts from the root directory
+dotenv.config({ path: "./.env" }); //starts from the root directory
 
 connectDB()
   .then(() => {
-    app.listen(process.env.port || 8000, () => {
+    app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at ${process.env.PORT}`);
     });
   })
